d39bed8e5e13acb6c112da0b91e129dde2f2c7b7
VictorAlcenio <48133710+VictorAlcenio@users.noreply.github.com>
2019-03-01 15:15:37 -0300

    Desenvolvimento do algoritmo

--- exercício_marceneiro.php
+++ exercício_marceneiro.php
@@ -2,6 +2,6 @@
 
 //Um marceneiro para fazer um trabalho, preisa cortar os pedaços de madeira com 45cm cada, ele pode comprar tabuas de 3,4 e 5 metros. Precisa saber a sobra de cada
 
-$madeira_3metros= 300/45;
+$quantidade_madeira= (int) (300/45);
 $madeira_3metrosS= 300%45;
-print "Se ele escolher a madeira de 3 metros comprará: $madeira_3metros, e sobrará: $madeira_3metrosS centimetros.";
+print "Se ele escolher a madeira de 3 metros comprará: $quantidade_madeira madeiras, e sobrará: $madeira_3metrosS centimetros.";
