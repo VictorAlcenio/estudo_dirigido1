ea4b1f624bac41eb9da08a6cf70bef24b4df003f
VictorAlcenio <48133710+VictorAlcenio@users.noreply.github.com>
2019-03-06 16:46:33 -0300

    continuação do desenvolvimento

--- exercício1.php
+++ exercício1.php
@@ -1,2 +1,11 @@
 <?php
 
+//Escreva um programa que leia um valor em metros e o exiba convertido em milímetros.
+
+print"Quantos metros voce deseja: 2 ou 5?";
+  
+$conversao_2metros_milimetros= 2*1000;
+$conversao_5metros_milimetros= 5*1000;
+  
+print"Se voce escolheu 2 metros, terá $conversao_2metros_milimetros milimetros\n";
+print"Se voce escolheu 5 metros, terá $conversao_5metros_milimetros milimetros";
