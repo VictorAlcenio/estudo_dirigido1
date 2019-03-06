f037210a093d285d7bea2504fd032f97e6f43fc1
VictorAlcenio <48133710+VictorAlcenio@users.noreply.github.com>
2019-03-06 16:51:19 -0300

    algumas alterações feitas para aprimorar o algoritmo

--- exercício1.php
+++ exercício1.php
@@ -2,10 +2,10 @@
 
 //Escreva um programa que leia um valor em metros e o exiba convertido em milímetros.
 
-print"Quantos metros voce deseja: 2 ou 5?";
+print"Quantos metros voce deseja: 2 ou 5?\n";
   
 $conversao_2metros_milimetros= 2*1000;
 $conversao_5metros_milimetros= 5*1000;
   
 print"Se voce escolheu 2 metros, terá $conversao_2metros_milimetros milimetros\n";
-print"Se voce escolheu 5 metros, terá $conversao_5metros_milimetros milimetros";
+print"Mas caso tenha escolhido 5 metros,  voce terá $conversao_5metros_milimetros milimetros";
