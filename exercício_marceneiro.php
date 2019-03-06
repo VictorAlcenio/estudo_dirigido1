cb2940b5065db387495ee56929370e27e0292d5e
VictorAlcenio <48133710+VictorAlcenio@users.noreply.github.com>
2019-03-06 16:12:16 -0300

    Continuação/conclusão do desenvolvimento do algoritmo

--- exercício_marceneiro.php
+++ exercício_marceneiro.php
@@ -1,7 +1,15 @@
 <?php
 
-//Um marceneiro para fazer um trabalho, preisa cortar os pedaços de madeira com 45cm cada, ele pode comprar tabuas de 3,4 e 5 metros. Precisa saber a sobra de cada
+//Um marceneiro para fazer um trabalho, precisa cortar os pedaços de madeira com 45cm cada, ele pode comprar tabuas de 3,4 e 5 metros. Precisa saber a sobra de cada
 
-$quantidade_madeira= (int) (300/45);
+$quantidade_madeira_3metros= (int) (300/45);
 $madeira_3metrosS= 300%45;
-print "Se ele escolher a madeira de 3 metros comprará: $quantidade_madeira madeiras, e sobrará: $madeira_3metrosS centimetros.";
+print "Se ele escolher a madeira de 3 metros comprará: $quantidade_madeira_3metros madeiras, e sobrará: $madeira_3metrosS centimetros.";
+
+$quantidade_madeira_4metros= (int) (400/45)
+$madeira_4metrosS= 400%45;
+print "Se ele escolher a madeira de 4 metros comprará: $quantidade_madeira_4metros madeiras, e sobrará: $madeira_4metrosS centimetros.";
+
+$quantidade_madeira_5metros= (int) (500/45)
+$madeira_5metrosS= 500%45;
+print "Se ele escolher a madeira de 5 metros comprará: $quantidade_madeira_5metros madeiras, e sobrará: $madeira_5metrosS centimetros.";
