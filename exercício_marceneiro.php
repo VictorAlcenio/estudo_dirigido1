ea3833f827c9a63c5e492a591c635cc1796a7db2
VictorAlcenio <48133710+VictorAlcenio@users.noreply.github.com>
2019-03-06 16:22:57 -0300

    Update exercício_marceneiro.php

--- exercício_marceneiro.php
+++ exercício_marceneiro.php
@@ -4,12 +4,12 @@
 
 $quantidade_madeira_3metros= (int) (300/45);
 $madeira_3metrosS= 300%45;
-print "Se ele escolher a madeira de 3 metros comprará: $quantidade_madeira_3metros madeiras, e sobrará: $madeira_3metrosS centimetros.";
+print "Se ele escolher a madeira de 3 metros comprará: $quantidade_madeira_3metros madeiras, e sobrará: $madeira_3metrosS centimetros.\n";
 
-$quantidade_madeira_4metros= (int) (400/45)
+$quantidade_madeira_4metros= (int) (400/45);
 $madeira_4metrosS= 400%45;
-print "Se ele escolher a madeira de 4 metros comprará: $quantidade_madeira_4metros madeiras, e sobrará: $madeira_4metrosS centimetros.";
+print "Se ele escolher a madeira de 4 metros comprará: $quantidade_madeira_4metros madeiras, e sobrará: $madeira_4metrosS centimetros.\n";
 
-$quantidade_madeira_5metros= (int) (500/45)
+$quantidade_madeira_5metros= (int) (500/45);
 $madeira_5metrosS= 500%45;
 print "Se ele escolher a madeira de 5 metros comprará: $quantidade_madeira_5metros madeiras, e sobrará: $madeira_5metrosS centimetros.";
