51a26e89f3d58d8659e75afd27e77f24db4a8fde
VictorAlcenio <48133710+VictorAlcenio@users.noreply.github.com>
2019-03-09 18:41:18 -0300

    execução e finalização do algoritmo

--- exercício7.php
+++ exercício7.php
@@ -1,5 +1,12 @@
 <?php
 
-//Escreva um programa que pergunte a quantidade de km percorridos por um carro alugado pelo usuário,
-assim como a quantidade de dias pelos quais o carro foi alugado. Calcule o preço a pagar, sabendo que o
-carro custa R$ 60,00 por dia e R$ 0,15 por km rodado.
+//Escreva um programa que pergunte a quantidade de km percorridos por um carro alugado pelo usuário,assim como a quantidade de dias pelos quais o carro foi alugado. Calcule o preço a pagar, sabendo que o carro custa R$ 60,00 por dia e R$ 0,15 por km rodado.
+
+print"Quantos quilometros foram percorridos pelo seu carro alugado?";
+$quilometros=(float)fgets(STDIN);
+print"Quantos dias voce ficou com ele?";
+$dias=(float)fgets(STDIN);
+
+$preco= ($dias*60)+($quilometros*0.15);
+print"Voce irá pagar $preco pelo aluguel do carro";
+
