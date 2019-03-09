ce58fecff2823118ada6d473139cab8b7157ba9f
VictorAlcenio <48133710+VictorAlcenio@users.noreply.github.com>
2019-03-09 17:06:32 -0300

    finalização do algoritmo

--- exercício2.php
+++ exercício2.php
@@ -2,13 +2,17 @@
 
 //Escreva um programa que leia a quantidade de dias, horas, minutos e segundos do usuário. Calcule o total em segundos.
 
-$quantidade_dias= 5;
-$quantidade_horas= 3;
-$quantidade_minutos= 45;
-$quantidade_segundos= 2;
+print"Escreva uma quantidade de dias:";
+$dias=(float)fgets(STDIN);
 
-print" O usuario tem: $quantidade_dias dias, $quantidade_horas horas, $quantidade_minutos minutos e $quantidade_segundos segundos\n";
+print"Escreva uma quantidade de horas:";
+$horas=(float)fgets(STDIN);
 
-$total_segundos= ($quantidade_dias*24*60*60) + ($quantidade_horas*60*60) + ($quantidade_minutos*60) + $quantidade_segundos;
-print" A quantidade total são: $total_segundos segundos";
+print"Escreva uma quantidae de minutos:";
+$minutos=(float)fgets(STDIN);
 
+print"Escreva uma quantidae de segundos:";
+$segundos=(float)fgets(STDIN);
+
+$total_segundos= ($dias*24*60*60)+($horas*60*60)+($minutos*60)+$segundos;
+print"A quantidade total de segundos obtidos nesse tempo foi de: $total_segundos segundos";
