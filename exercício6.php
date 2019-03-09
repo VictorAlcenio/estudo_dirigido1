7755110c450ecd6a7507e44fe4b473c60a9f288d
VictorAlcenio <48133710+VictorAlcenio@users.noreply.github.com>
2019-03-09 18:14:56 -0300

    execução e finalização do algoritmo

--- exercício6.php
+++ exercício6.php
@@ -1,3 +1,10 @@
 <?php
 
 //Converta uma temperatura digitada em Celsius para Fahrenheit. F = 9*C/5 + 32. 
+
+print"Digite uma temperatura em graus celsius:";
+$temperatura_Celsius=(float)fgets(STDIN);
+
+$temperatura_Fahrenheit= (9*$temperatura_Celsius)/5 + 32;
+print"A temperatura convertida será de $temperatura_Fahrenheit Fahrenheit";
+                                  
