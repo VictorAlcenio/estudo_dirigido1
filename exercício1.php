37c045f96bcf43000bcae5a95803d8db1000ccea
VictorAlcenio <48133710+VictorAlcenio@users.noreply.github.com>
2019-03-09 16:54:21 -0300

    finalização do algoritmo

--- exercício1.php
+++ exercício1.php
@@ -2,10 +2,8 @@
 
 //Escreva um programa que leia um valor em metros e o exiba convertido em milímetros.
 
-print"Quantos metros voce deseja: 2 ou 5?\n";
-  
-$conversao_2metros_milimetros= 2*1000;
-$conversao_5metros_milimetros= 5*1000;
-  
-print"Se voce escolheu 2 metros, terá $conversao_2metros_milimetros milimetros\n";
-print"Mas caso tenha escolhido 5 metros,  voce terá $conversao_5metros_milimetros milimetros";
+print"Quantos metros você quer converter?";
+$metros= (float)fgets(STDIN);
+
+$conversao= $metros*1000;
+print"Sua conversão foi de $conversao milimetros.";
