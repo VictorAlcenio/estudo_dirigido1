5a839bd05bbbbf49663bb6b6f1732e26e935b50e
VictorAlcenio <48133710+VictorAlcenio@users.noreply.github.com>
2019-03-09 23:54:35 -0300

    execução e finalização do algoritmo

--- exercício8.php
+++ exercício8.php
@@ -1,8 +1,6 @@
 <?php
 
-//Escreva um programa para calcular a redução do tempo de vida de um fumante. Pergunte a quantidade
-de cigarros fumados por dia e quantos anos ele já fumou. Considere que um fumante perde 10 minutos
-de vida a cada cigarro, calcule quantos dias de vida um fumante perderá. Exiba o total de dias.
+//Escreva um programa para calcular a redução do tempo de vida de um fumante. Pergunte a quantidadede cigarros fumados por dia e quantos anos ele já fumou. Considere que um fumante perde 10 minutosde vida a cada cigarro, calcule quantos dias de vida um fumante perderá. Exiba o total de dias.
   
 print"Quantos cigarros voce fuma por dia?";
 $quantidade_cigarros_dia=(float)fgets(STDIN);
@@ -10,8 +8,8 @@ $quantidade_cigarros_dia=(float)fgets(STDIN);
 print"Quantos anos voce já fuma?";
 $quantidade_anos=(float)fgets(STDIN);
 
-$quantidade_dias= $quantidade_cigarros_dia*10*365*$quantidade_anos;
-$minutos_perdidos= $quantidade_dias*11;
+$quantidade_dias= $quantidade_cigarros_dia*365*$quantidade_anos;
+$minutos_perdidos= $quantidade_dias*10;
 $horas_perdidas= $minutos_perdidos/60;
 $dias_perdidos= round($horas_perdidas/24);
 
