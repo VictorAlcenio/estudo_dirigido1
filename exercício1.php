8a5a813111a2fca3c5d811c98d26fe57a63b7a6e
Victor <victoralcenio.loh@gmail.com>
2019-03-14 23:12:46 -0300

    Inserir espaço.

--- exercício1.php
+++ exercício1.php
@@ -3,7 +3,7 @@
 //Escreva um programa que leia um valor em metros e o exiba convertido em milímetros.
 
 print "Quantos metros você quer converter?";
-$metros= (float)fgets(STDIN);
+$metros = (float)fgets(STDIN);
 
 $conversao= $metros*1000;
 print"Sua conversão foi de $conversao milimetros.";
