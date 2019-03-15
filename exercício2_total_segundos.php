402ce92e3fbb66e414588b508c8b033d50bac307
VictorAlcenio <48133710+VictorAlcenio@users.noreply.github.com>
2019-03-14 23:45:58 -0300

    inseri espaços e fiz alteração do nome

--- exercício2_total_segundos.php
+++ exercício2_total_segundos.php
@@ -2,17 +2,19 @@
 
 //Escreva um programa que leia a quantidade de dias, horas, minutos e segundos do usuário. Calcule o total em segundos.
 
-print"Escreva uma quantidade de dias:";
+print "Escreva uma quantidade de dias: ";
 $dias=(float)fgets(STDIN);
 
-print"Escreva uma quantidade de horas:";
+print "Escreva uma quantidade de horas: ";
 $horas=(float)fgets(STDIN);
 
-print"Escreva uma quantidae de minutos:";
+print "Escreva uma quantidade de minutos: ";
 $minutos=(float)fgets(STDIN);
 
-print"Escreva uma quantidae de segundos:";
+print "Escreva uma quantidade de segundos: ";
 $segundos=(float)fgets(STDIN);
 
 $total_segundos= ($dias*24*60*60)+($horas*60*60)+($minutos*60)+$segundos;
 print"A quantidade total de segundos obtidos nesse tempo foi de: $total_segundos segundos";
+
+?>
